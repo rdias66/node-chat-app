3f6d00faa4e0aa451f8ea5c41cde8b8ba52f42c7
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-04-25 10:24:56 -0300

    Update and rename message.view.js to message.routes.js

---
+++ src/message/message.routes.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import { createMessage ,getMessagesById, getAllMessages  } from './controller.js'
+
+export const messageRoutes = express.Router()
+
+messageRoutes.use(express.json())
+
+messageRoutes.post('/createMessage', createMessage)
+
+messageRoutes.get('/getMessageById', getMessagesById)
+
+messageRoutes.get('/getAllMessages', getAllMessages)
